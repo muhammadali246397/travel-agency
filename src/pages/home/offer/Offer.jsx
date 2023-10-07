251f2a9d5279e1dc62b529774e259fd9073a0bc1
muhammadali246397 <muhammadali246397@gmail.com>
2023-10-07 22:00:51 +0600

    animation add in offer section

--- src/pages/home/offer/Offer.jsx
+++ src/pages/home/offer/Offer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import cuple from '../../../assets/cuple.jpg'
 import turist from '../../../assets/turist.jpg'
+import './Offer.css'
+import 'animate.css';
+
 
 const Offer = () => {
     return (
@@ -10,15 +13,28 @@ const Offer = () => {
                     <div>
                         <h2 data-aos='fade-down' className='text-[#FB8E26] text-3xl mb-6'>Special Offer For Couples</h2>
                         <p data-aos='fade-up' className='text-[#A1A1A1] mb-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor  ut et dolore magna aliqua.</p>
-                        <div>
-                            <img className='hover:scale-90 transition-transform' src={cuple} alt="" />
+                        <div className='relative effect'>
+                            <img data-aos="flip-left"
+                                data-aos-easing="ease-out-cubic"
+                                data-aos-duration="1000" className='offerimg' src={cuple} alt="" />
+                            <div className='overlay px-10'>
+                                <div className=''>
+                                    <h2 className='text-3xl font-semibold text-white'>Discount up to 50%</h2>
+                                    <p className='text-white text-lg mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eligendi est ex omnis cupiditate harum.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div>
-                        <div>
-                            <img className='hover:scale-90 transition-transform' src={turist} alt="" />
-                            <div>
-                                
+                        <div className='relative effect'>
+                            <img data-aos="flip-right"
+                                data-aos-easing="ease-out-cubic"
+                                data-aos-duration="1000" className='offerimg' src={turist} alt="" />
+                            <div className='overlay px-10'>
+                                <div className=''>
+                                    <h2 className='text-3xl font-semibold text-white'>Discount up to 50%</h2>
+                                    <p className='text-white text-lg mt-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, sunt alias dignissimos minus maiores pariatur!</p>
+                                </div>
                             </div>
                         </div>
                         <h2 data-aos-delay='500' data-aos='fade-down' className='text-[#FB8E26] text-3xl mt-6'>Special Offer On Adventure Tours</h2>
